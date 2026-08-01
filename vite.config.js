0b2371a4ec45fac8ba2007498552de8d2e95d2d1
mehdizero740-boop <284680659+mehdizero740-boop@users.noreply.github.com>
2026-08-01 14:47:03 +0200

    optimisation build

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("src/data/questions.js")) return "questions-data";
+          if (id.includes("src/data/courses.js")) return "courses-data";
+          if (id.includes("node_modules")) return "vendor";
+        },
+      },
+    },
+  },
 });
+
